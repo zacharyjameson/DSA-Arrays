96b23937da74aca3b0a892f3e912dd499a706266
Zack Jameson <zacharymjameson@gmail.com>
2021-05-23 08:50:16 -0400

    first commit

---
+++ drills.js
@@ -0,0 +1,113 @@
+const Memory = require('./memory')
+const memory = new Memory();
+
+class Memory {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error("Out of memory");
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length == 0) {
+      throw new Error("Index error");
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+    memory.copy(
+      this.ptr + index,
+      this.ptr + index + 1,
+      this.length - index - 1
+    );
+    this.length--;
+  }
+
+  allocate(size) {
+    if (this.head + size > this.memory.length) {
+      return null;
+    }
+
+    let start = this.head;
+
+    this.head += size;
+    return start;
+  }
+
+  free(ptr) {}
+
+  copy(toIdx, fromIdx, size) {
+    if (fromIdx === toIdx) {
+      return;
+    }
+
+    if (fromIdx > toIdx) {
+      // Iterate forwards
+      for (let i = 0; i < size; i++) {
+        this.set(toIdx + i, this.get(fromIdx + i));
+      }
+    } else {
+      // Iterate backwards
+      for (let i = size - 1; i >= 0; i--) {
+        this.set(toIdx + i, this.get(fromIdx + i));
+      }
+    }
+  }
+
+  get(ptr) {
+    return this.memory[ptr];
+  }
+
+  set(ptr, value) {
+    this.memory[ptr] = value;
+  }
+}
+
+module.exports = Memory;
---
+++ index.js
@@ -0,0 +1,23 @@
+const Array = require("./memory");
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  let arr = new Array();
+
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+
+  arr.pop();
+  arr.pop();
+  arr.pop();
+
+  console.log(arr.get(0));
+  console.log(arr);
+}
+
+main();
